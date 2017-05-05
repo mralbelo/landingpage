a04b2ab215c3866b319043255605b6d84c35e3d8
Juan Albelo <Albelo@Juans-MacBook-Pro.local>
2017-05-04 20:52:00 -0500

    ...

--- js/feed.js
+++ js/feed.js
@@ -9,5 +9,8 @@
 
 
 //620292069.1677ed0.07289f2dff8544c3a3a119da942fdfaa
-
+//620292069.c117479.f68652eca49945c4b175164ed0994bfb
 //09343d6cd88f4d2fa3092ec75f985bb6
+
+
+https://www.instagram.com/oauth/authorize/?client_id=c1174798a7284cdf8170a043511be976&redirect_uri=http://localhost:3000&response_type=token&scope=public_content
