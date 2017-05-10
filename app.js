ecd83818f55cd36b35a47826098ff75489e23d27
Christian Vazquez <colapsov@gmail.com>
2017-05-09 20:04:38 -0500

    add vendor changes

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 var app = angular.module('mralbelo', []);
 
 
-app.controller('FeedController', function FeedController($scope) {
+app.controller('FeedController', function FeedController($scope,$http) {
   
-
+        $http.get().then(function(response){
+            $scope.feed = result;
+        });
     console.log("feed controller was initialize");
 
 });
